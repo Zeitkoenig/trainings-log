b711b545024f96ca0c704973eefb1910b1ccbc9f
Zeitkoenig <email@freizeitkoenig.de>
2026-07-08 10:39:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'trainings-log-v2';
+const CACHE = 'trainings-log-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
